81975d7b4caf903de9673e54bbf11ddf34af3fe4
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2025-05-05 00:49:35 +0300

    video 139 FİNİSH
    video 139 FİNİSH

--- app/news/page.js
+++ app/news/page.js
@@ -1,13 +1,21 @@
 import Link from "next/link";
 
+import { DUMMY_NEWS } from '@/dummy-news';
+
+
 export default function NewsPage() {
     return (
         <>
             <h1>   NEWS PAGE</h1>
-            <ul>
-                <li><Link href="/news/first-news">First News Item</Link></li>
-                <li><Link href="/news/second-news">Second News Item</Link></li>
-                <li><Link href="/news/third-news">Third News Item</Link></li>
+            <ul className="news-list">
+                {DUMMY_NEWS.map((newsItem) => (
+                    <li key={newsItem.id}>
+                        <Link href={`/news/${newsItem.slug}`}>
+                            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                            <span>{newsItem.title}</span>
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </>
     )

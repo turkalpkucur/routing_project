175f45948b1db8b777b8e0f4b1d70ac92aa60c5e
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2025-05-08 00:21:27 +0300

    140 finish
    140 finish

---
+++ app/news/not-found.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function NewsNotFoundPage() {
+    return <div id="error">
+        <h1>NOT FOUND</h1>
+        <p>unfortunately we could not find requested article!</p>
+    </div>
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function NotFoundPage() {
+    return <div id="error">
+        <h1>NOT FOUND</h1>
+        <p>the requested resource could not be found!</p>
+    </div>
+}

258dbb7497e19eaed2ad815e1e807f5bdae525ad
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2025-05-27 01:25:56 +0300

    modal eklendi çalışıyor
    modal eklendi çalışıyor

--- app/news/[slug]/(.)image/page.js
+++ app/news/[slug]/(.)image/page.js
@@ -10,10 +10,12 @@ export default function InterceptedImagePage({ params }) {
     }
     return (
         <>
-            <h2>Intercepted!.</h2>
-            <div className="fullscreen-image">
-                <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-            </div>
+            <div className="modal-backdrop" />
+            <dialog className="modal" open>
+                <div className="fullscreen-image">
+                    <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                </div>
+            </dialog>
         </>
     )
 }

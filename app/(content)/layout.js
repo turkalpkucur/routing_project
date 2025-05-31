b529096ef19e23622b17964d486c251bfe271752
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2025-05-31 21:42:00 +0300

    154 FINISH
    154 FINISH

---
+++ app/(content)/layout.js
@@ -0,0 +1,16 @@
+import MainHeader from "@/components/main-header";
+import "../globals.css";
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body>
+                <div id="page">
+                    <MainHeader />
+                    {children}
+                </div>
+
+            </body>
+        </html>
+    );
+}
--- app/(content)/news/[slug]/(.)image/page.js
+++ app/(content)/news/[slug]/(.)image/page.js
@@ -1,8 +1,15 @@
+"use client"
 import { DUMMY_NEWS } from "@/dummy-news";
 import { notFound } from "next/navigation";
+import { useRouter } from "next/router";
 
 export default function InterceptedImagePage({ params }) {
+
+    const router = useRouter();
+
     const newsItemSlug = params.slug;
+
+
 
     const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsItemSlug)
     if (!newsItem) {
@@ -10,7 +17,7 @@ export default function InterceptedImagePage({ params }) {
     }
     return (
         <>
-            <div className="modal-backdrop" />
+            <div className="modal-backdrop" onClick={router.back} />
             <dialog className="modal" open>
                 <div className="fullscreen-image">
                     <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />

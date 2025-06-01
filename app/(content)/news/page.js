e197fa303076d4e0335194c0384038b17b7292b0
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2025-06-02 00:29:55 +0300

    161 finish server side fetching
    161 finish server side fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,47 +1,16 @@
-
-"use client"
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
 
+export default async function NewsPage() {
+    const response = await fetch('http://localhost:8080/news');
 
-export default function NewsPage() {
-
-    const [news, setNews] = useState();
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState();
-    useEffect(() => {
-        async function fetchNews() {
-            setIsLoading(true);
-            const response = await fetch('http://localhost:8080/news');
-
-            if (!response.ok) {
-                setError('Failed to fetch news ');
-                setIsLoading(false);
-            }
-
-            const news = await response.json();
-            setIsLoading(false);
-            setNews(news);
-        }
-
-        fetchNews();
-    }, []);
-
-    if (isLoading) {
-        return <p>Loading...</p>;
+    if (!response.ok) {
+        throw new Error('Failed to fetch news.');
     }
-    if (error) {
-        return <p>{error}</p>;
-    }
-    let newsContent;
-    if (news) {
-        newsContent = <NewsList news={news} />
-    }
-
+    const news = await response.json();
     return (
         <>
             <h1>   NEWS PAGE</h1>
-            {newsContent}
+            <NewsList news={news} />
         </>
     )
 }

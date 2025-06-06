03c8af69602e6ae0dd3d7b36824879d9a69bb54f
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2025-06-06 22:30:45 +0300

    162 finish
    162 finish

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,12 +1,9 @@
 import NewsList from "@/components/news-list";
+import { getAllNews } from "@/lib/news";
 
 export default async function NewsPage() {
-    const response = await fetch('http://localhost:8080/news');
 
-    if (!response.ok) {
-        throw new Error('Failed to fetch news.');
-    }
-    const news = await response.json();
+    const news = getAllNews();
     return (
         <>
             <h1>   NEWS PAGE</h1>
